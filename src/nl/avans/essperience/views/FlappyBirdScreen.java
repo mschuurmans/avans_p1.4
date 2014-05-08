b16e20d5caf93f6a51c4acd774f6f42def5ce995
Bart Salters <bmsalter@avans.nl>
2014-05-08 17:03:42 +0200

    test subimg in class flappybird

--- src/nl/avans/essperience/views/FlappyBirdScreen.java
+++ src/nl/avans/essperience/views/FlappyBirdScreen.java
@@ -1,17 +1,24 @@
 package nl.avans.essperience.views;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 import nl.avans.essperience.models.GameModel;
+import nl.avans.essperience.utils.AssetManager;
 
 public class FlappyBirdScreen extends GameScreen
 {
+	private BufferedImage _background;
+	private BufferedImage _spritesheet;
 	private static final long serialVersionUID = -2013215913618586135L;
 
 	public FlappyBirdScreen(GameModel model) 
 	{
 		super(model);
 		// TODO Auto-generated constructor stub
+		_spritesheet = (BufferedImage) AssetManager.Instance().getImage("flappy.png");
+		//_background = spritesheet.getSubimage(288, 512, 288, 512);
 	}
 
 	@Override
@@ -24,8 +31,11 @@ public class FlappyBirdScreen extends GameScreen
 	@Override
 	public void paintComponent(Graphics g) 
 	{
-		g.drawString("FlappyBird Screen", 10, 15);
+		super.paintComponents(g);
+		Graphics2D g2 = (Graphics2D)g;
 		
+		g2.drawString("FlappyBird Screen", 10, 15);
+		g2.drawImage(_spritesheet, 0, 0, 288, 512, null);
 		super.drawLives(g);
 	}
 
