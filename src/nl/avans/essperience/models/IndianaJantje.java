058900d9595dd6822ab4dd7ea8916c48780a814d
rudyjason <rudyjason.ep@hotmail.com>
2014-05-08 13:17:10 +0200

    skeleton indiana jantje

---
+++ src/nl/avans/essperience/models/IndianaJantje.java
@@ -0,0 +1,12 @@
+package nl.avans.essperience.models;
+
+public class IndianaJantje extends GameModel
+{
+	private int _maxTime;
+	private int _speed;
+	
+	IndianaJantje(int maxTime, int speed) {
+		this._maxTime = maxTime;
+		this._speed = speed;
+	}
+}
