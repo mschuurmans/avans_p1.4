3cced7b33b2414b3e89aad4c795471d65e901cda
rudyjason <rudyjason.ep@hotmail.com>
2014-05-09 11:24:44 +0200

    push voor stefantje

--- src/nl/avans/essperience/views/IndianaJantjeScreen.java
+++ src/nl/avans/essperience/views/IndianaJantjeScreen.java
@@ -13,7 +13,11 @@ public class IndianaJantjeScreen extends GameScreen
 	// Images
 	private BufferedImage _spritesheet;
 	private Image _background;
+	private Image _stone;
 	private int _side;
+	private double _offset;
+	private int _drawX;
+	private int _drawY;
 	
 	private static final long serialVersionUID = -2013215913618586135L;
 
@@ -21,6 +25,8 @@ public class IndianaJantjeScreen extends GameScreen
 	{
 		super(model);
 		// TODO Auto-generated constructor stub
+		_background = AssetManager.Instance().getImage("IndianaJantje/background.jpg");
+		_stone = AssetManager.Instance().getImage("Flappy/flappy.png");
 		init();
 	}
 
@@ -35,10 +41,13 @@ public class IndianaJantjeScreen extends GameScreen
 		switch (_side)
 		{
 			case 0:
+				_offset = 0;
 				break;
 			case 1:
+				_offset = 1;
 				break;
 			case 2:
+				_offset = -1;
 				break;
 			default: System.out.println("OMFG SIDE WENT WRONG");
 				break;
@@ -49,6 +58,15 @@ public class IndianaJantjeScreen extends GameScreen
 	public void paintComponent(Graphics g) 
 	{
 		super.paintComponent(g);
+		int screenWidth = Main.GAME.getWidth();
+		int screenHeight = Main.GAME.getHeight();
+		_drawY = screenHeight/2;
+		_drawX = (int) ((screenWidth/2) + ((screenWidth/4)*_offset));
+		
+		int backgroundWidth = _background.getWidth(null);
+		g.drawImage(_background, 0, 0, screenWidth, screenHeight, null);
+		g.drawImage(_stone, _drawX, _drawY, screenWidth/2, screenHeight/2, null);
+		init();
 	}
 	
 	private int chooseSide() {
