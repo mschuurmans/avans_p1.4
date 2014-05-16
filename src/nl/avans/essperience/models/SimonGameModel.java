c2992d9d21e1999b1a560ac0d18222c45b143f37
Jack <jackxl@hotmail.com>
2014-05-16 16:52:25 +0200

    Changes in SimonGameModel and entities

--- src/nl/avans/essperience/models/SimonGameModel.java
+++ src/nl/avans/essperience/models/SimonGameModel.java
@@ -1,31 +1,45 @@
 package nl.avans.essperience.models;
 
+import java.awt.Image;
+
 import net.phys2d.math.Vector2f;
 import net.phys2d.raw.Body;
 import net.phys2d.raw.StaticBody;
 import net.phys2d.raw.World;
 import net.phys2d.raw.shapes.Line;
 import net.phys2d.raw.strategies.QuadSpaceStrategy;
+import nl.avans.essperience.entities.simon.FruitPiece;
 import nl.avans.essperience.main.Main;
+import nl.avans.essperience.utils.AssetManager;
 
 public class SimonGameModel extends GameModel
 {
+	public static final int BANANA = 0;
+	public static final int ORANGE = 1;
+	public static final int APPLE = 2;
+	public static final int PEAR = 3;
+	
 	private int _patternLenght;
 	private World _myWorld;
 	private Body _floor;
 	private Body[] _fruitPieces;
 	
+	private Image[] _fruitImages;
+	
 	public SimonGameModel()
-	{
+	{		
+		_fruitImages[BANANA] = AssetManager.Instance().getImage("Simon/banana.png");
+		_fruitImages[ORANGE] = AssetManager.Instance().getImage("Simon/orange.png");
+		_fruitImages[APPLE] = AssetManager.Instance().getImage("Simon/apple.png");
+		_fruitImages[PEAR] = AssetManager.Instance().getImage("Simon/pear.png");
 		
 		init();
-		
 	}
 
 	private void init()
 	{
 		int _difficulty = Main.GAME.getDifficulty();
-		_patternLenght = _difficulty /2;
+		_patternLenght = (_difficulty /2) +1;
 		
 		_myWorld = new World(new Vector2f(0.0f, 10.0f), 10, new QuadSpaceStrategy(20,5));
 		
@@ -41,9 +55,13 @@ public class SimonGameModel extends GameModel
 		
 		_myWorld.add(_floor);
 		
-		for(int i = 0; i < _patternLenght; i++)
+		for(int i = 0; i < _patternLenght; i++)							//add pieces of fruit
 		{
-//			_fruitPieces[i] = new fruitpiece
+			FruitPiece fp = new FruitPiece();							//create new pieces of fruit
+			_fruitPieces[i] = fp.getBody();								//get the Bodies and store them
+			_fruitPieces[i].setPosition(Main.GAME.getWidth()/2, 0);		//position these bodies at the top center
+			
+			_myWorld.add(_fruitPieces[i]);								// add fruitpieces to the world
 		}
 	}
 	
@@ -52,8 +70,20 @@ public class SimonGameModel extends GameModel
 		_myWorld.step();
 	}
 	
-	
-	
+	public Image getFruitImage(String name)
+	{
+		switch(name)
+		{
+		default: // case "banana"
+			return _fruitImages[BANANA];
+		case "orange":
+			return _fruitImages[ORANGE];
+		case "apple":
+			return _fruitImages[APPLE];
+		case "pear":
+			return _fruitImages[PEAR];
+		}
+	}
 	
 }
 
