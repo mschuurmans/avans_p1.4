3101d3813e4a32ec65aedcf75a960eeb30142e27
Michiel Schuurmans <michiel@codox.org>
2014-05-19 09:15:42 +0200

    changes

--- src/nl/avans/essperience/utils/AssetManager.java
+++ src/nl/avans/essperience/utils/AssetManager.java
@@ -11,6 +11,8 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
 
 public class AssetManager
 {
