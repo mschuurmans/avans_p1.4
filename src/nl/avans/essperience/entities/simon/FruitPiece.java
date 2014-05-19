947e9f1fa4fab9fbcf583753f4e68f5132eaafd5
Jack <jackxl@hotmail.com>
2014-05-19 18:38:28 +0200

    Changes to Simon Loves Fruit. now have physics working. still needs work though

--- src/nl/avans/essperience/entities/simon/FruitPiece.java
+++ src/nl/avans/essperience/entities/simon/FruitPiece.java
@@ -2,12 +2,15 @@ package nl.avans.essperience.entities.simon;
 
 import net.phys2d.raw.Body;
 import net.phys2d.raw.shapes.Box;
+import nl.avans.essperience.main.Main;
 
 public class FruitPiece
 {
 	String _name;
 	Box _shape;
 	float _m;
+	
+	int _massModifier = 10000;
 	
 	/**
 	 * creates a random FruitPiece for the Simon Loves Fruit game
@@ -40,49 +43,23 @@ public class FruitPiece
 		switch(name)
 		{
 		case "banana":
-			_shape = new Box(20f, 10f);
-			_m = 126/1000;
+			_shape = new Box(80f, 40f);
+			_m = 126*_massModifier;
 			break;
 		case "orange":
-			_shape = new Box(15f, 15f);
-			_m = 54/1000;
+			_shape = new Box(60f, 60f);
+			_m = 54*_massModifier;
 			break;
 		case "apple":
-			_shape = new Box(15f, 15f);
-			_m = 70/1000;
+			_shape = new Box(60f, 60f);
+			_m = 70*_massModifier;
 			break;
 		case "pear":
-			_shape = new Box(10f, 15f);
-			_m = 165/1000;
+			_shape = new Box(60f, 60f);
+			_m = 165*_massModifier;
 			break;
 		}
 		
-	}
-	
-	public FruitPiece(String name)
-	{
-		_name = name;
-		
-		switch(name)
-		{
-		case "banana":
-			_shape = new Box(20f, 10f);
-			_m = 126/1000;
-			break;
-		case "orange":
-			_shape = new Box(15f, 15f);
-			_m = 54/1000;
-			break;
-		case "apple":
-			_shape = new Box(15f, 15f);
-			_m = 70/1000;
-			break;
-		case "pear":
-			_shape = new Box(10f, 15f);
-			_m = 165/1000;
-			break;
-		}
-
 	}
 	
 	public String getName()
@@ -104,6 +81,13 @@ public class FruitPiece
 	{
 		Body body = new Body(_name, _shape, _m);
 		body.setUserData(new String(getName()));
+		
+		int randX = (int)(Math.random() * Main.GAME.getWidth()/3 ) + (Main.GAME.getWidth()/3) +1;
+		
+		body.setPosition(randX, 0f);		//position these bodies at the top center
+		body.setFriction(1f);
+		body.setRestitution(0.1f);
+		
 		return body;
 	}
 
--- src/nl/avans/essperience/models/SimonGameModel.java
+++ src/nl/avans/essperience/models/SimonGameModel.java
@@ -7,10 +7,9 @@ import java.util.List;
 
 import net.phys2d.math.Vector2f;
 import net.phys2d.raw.Body;
-import net.phys2d.raw.BodyList;
 import net.phys2d.raw.StaticBody;
 import net.phys2d.raw.World;
-import net.phys2d.raw.shapes.Line;
+import net.phys2d.raw.shapes.Box;
 import net.phys2d.raw.strategies.QuadSpaceStrategy;
 import nl.avans.essperience.entities.simon.FruitPiece;
 import nl.avans.essperience.main.Main;
@@ -30,6 +29,8 @@ public class SimonGameModel extends GameModel
 	private boolean _debug = true;
 	private Image[] _fruitImages = new Image[4];
 	
+	private int _stepsPerUpdate;
+	
 	public SimonGameModel()
 	{		
 		_fruitImages[BANANA] = AssetManager.Instance().getImage("Simon/banana.png");
@@ -44,19 +45,16 @@ public class SimonGameModel extends GameModel
 	{
 		int _difficulty = Main.GAME.getDifficulty();
 		_patternLenght = (_difficulty /4) +3;
+		_patternLenght = 40;
+		_stepsPerUpdate = 5;
 		
 		_myWorld = new World(new Vector2f(0.0f, 10.0f), 10, new QuadSpaceStrategy(20,5));
 		
 		_myWorld.clear();
 		_myWorld.setGravity(0, 30);
 		
-		int x1 = 0;
-		int x2 = Main.GAME.getWidth();
-		int y1 = Main.GAME.getHeight() -20;
-		int y2 = y1;
-		
-		_floor = new StaticBody("Floor", new Line(x1, y1, x2, y2));
-		
+		_floor = new StaticBody("Floor", new Box(Main.GAME.getWidth(), 1f));
+		_floor.setPosition(Main.GAME.getWidth()/2, Main.GAME.getHeight() - 200);
 		_myWorld.add(_floor);
 		
 		for(int i = 0; i < _patternLenght; i++)					//add pieces of fruit
@@ -67,8 +65,6 @@ public class SimonGameModel extends GameModel
 		
 		for(Body body : _fruitPieces)
 		{
-			body.setPosition(Main.GAME.getWidth()/2f, 0f);		//position these bodies at the top center
-			body.setRestitution(0.7f);
 			_myWorld.add(body);									// add fruitpieces to the world
 		}
 		
@@ -82,7 +78,9 @@ public class SimonGameModel extends GameModel
 
 		System.out.println("position: " + _fruitPieces.get(0).getPosition().toString());
 		super.update();
-		_myWorld.step();
+		
+		for(int i = 0; i < _stepsPerUpdate; i ++)
+			_myWorld.step();
 		
 	}
 	
@@ -90,7 +88,28 @@ public class SimonGameModel extends GameModel
 	{
 		for(Body body : _fruitPieces)
 		{			
-			g.drawImage(getFruitImage(body), (int)body.getPosition().getX(), (int)body.getPosition().getY(), null);
+			g.drawImage(getFruitImage(body), (int)body.getPosition().getX() -32, (int)body.getPosition().getY(), 64, 64, null);
+		}
+		
+
+		if(_debug)
+		{
+			//debug display Floor
+			int x = (int) _floor.getPosition().getX() - Main.GAME.getWidth()/2;
+			int y = (int) _floor.getPosition().getY();
+			int w = (int)_floor.getShape().getBounds().getWidth();
+			int h = (int)_floor.getShape().getBounds().getHeight();
+			g.drawRect(x, y, w, h);
+			
+			//debug display bodyBoundingBoxes
+			for(Body body : _fruitPieces)
+			{
+				int bw = (int) body.getShape().getBounds().getWidth();
+				int bh = (int) body.getShape().getBounds().getHeight();
+				int bx = (int) body.getPosition().getX() - bw/2;
+				int by = (int) body.getPosition().getY();
+				g.drawRect(bx, by, bw, bh);
+			}
 		}
 	}
 	
