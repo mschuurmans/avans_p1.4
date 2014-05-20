0f7b5193b6a09cc05688d52d2308715e2a9e83eb
Michiel Schuurmans <michiel@codox.org>
2014-05-20 15:41:06 +0200

    Added game over screen

--- src/nl/avans/essperience/controllers/GameHandler.java
+++ src/nl/avans/essperience/controllers/GameHandler.java
@@ -7,22 +7,22 @@ import nl.avans.essperience.events.MicroGameFinishedEventListener;
 import nl.avans.essperience.main.Main;
 import nl.avans.essperience.models.FlappyBirdModel;
 import nl.avans.essperience.models.GameModel;
+import nl.avans.essperience.models.GameOverModel;
 import nl.avans.essperience.models.IndianaJantjeModel;
 import nl.avans.essperience.models.MenuModel;
 import nl.avans.essperience.models.RedButtonModel;
 import nl.avans.essperience.models.ScoreModel;
 import nl.avans.essperience.models.SimonGameModel;
-import nl.avans.essperience.models.TestModel;
 import nl.avans.essperience.models.WafModel;
 import nl.avans.essperience.utils.AssetManager;
 import nl.avans.essperience.views.FlappyBirdScreen;
+import nl.avans.essperience.views.GameOverScreen;
 import nl.avans.essperience.views.GameScreen;
 import nl.avans.essperience.views.IndianaJantjeScreen;
 import nl.avans.essperience.views.MenuScreen;
 import nl.avans.essperience.views.RedButtonScreen;
 import nl.avans.essperience.views.ScoreScreen;
 import nl.avans.essperience.views.SimonGameScreen;
-import nl.avans.essperience.views.TestScreen;
 import nl.avans.essperience.views.WafScreen;
 
 public class GameHandler extends JFrame
@@ -124,17 +124,30 @@ public class GameHandler extends JFrame
 		init(false);
 	}
 
+	public void gameOver()
+	{
+		this._gameModel = new GameOverModel();
+		this._gameScreen = new GameOverScreen((GameOverModel) _gameModel);
+		this._gameController = new GameOverController((GameOverModel) _gameModel, (GameOverScreen) _gameScreen);
+		changeScreen();
 
+		((GameOverController)_gameController).start();
+	}
 
 	public void nextGame(boolean succeed)
 	{
+		if(_gameController instanceof GameOverController)
+		{
+			reset();
+			return;
+		}
 		//System.out.println("GOING TO CHANGE THE SCREEN");
 		System.out.println(succeed);
 		if(!succeed)
 		{
 			if(_lives == 1)
 			{
-				reset();
+				gameOver();
 				return;
 			}	
 			else
@@ -150,7 +163,7 @@ public class GameHandler extends JFrame
 		}
 		else
 		{
-			int rand = (int) (Math.random() * _NUMBEROFGAMES) +5 ;
+			int rand = (int) (Math.random() * _NUMBEROFGAMES) +3 ;
 			switch (rand) 
 			{
 				case 1: 
@@ -179,9 +192,9 @@ public class GameHandler extends JFrame
 					this._gameController = new SimonGameController((SimonGameModel) _gameModel, (SimonGameScreen)_gameScreen);
 					break;
 				case 6:
-					this._gameModel = new TestModel();
-					this._gameScreen = new TestScreen((TestModel)_gameModel);
-					this._gameController = new TestController((TestModel)_gameModel, (TestScreen)_gameScreen);
+					this._gameModel = new GameOverModel();
+					this._gameScreen = new GameOverScreen((GameOverModel)_gameModel);
+					this._gameController = new GameOverController((GameOverModel)_gameModel, (GameOverScreen)_gameScreen);
 					break;
 				default:
 					reset();
---
+++ src/nl/avans/essperience/controllers/GameOverController.java
@@ -0,0 +1,56 @@
+package nl.avans.essperience.controllers;
+
+import nl.avans.essperience.events.InputTriggerdEventListener;
+import nl.avans.essperience.main.Main;
+import nl.avans.essperience.models.GameOverModel;
+import nl.avans.essperience.models.ScoreModel;
+import nl.avans.essperience.utils.Enums.GameKeys;
+import nl.avans.essperience.views.GameOverScreen;
+import nl.avans.essperience.views.ScoreScreen;
+
+public class GameOverController extends GameController 
+{
+	private GameOverModel _model;
+	private GameOverScreen _view;
+	
+	public GameOverController(GameOverModel model, GameOverScreen view)
+	{
+		this._model = model;
+		this._view = view;
+		
+		InputController.Instance().addInputTriggeredEventListener(new InputTriggerdEventListener()
+		{ 
+			public void keyPressed(GameKeys key)
+			{
+				
+			}
+		});
+		
+		InputController.Instance().setMotionDetecting(false);
+		_view.addKeyListener(InputController.Instance().getKeyboardListener());
+	}
+	
+	public void start()
+	{
+		Thread t = new Thread(new Runnable()
+		{
+
+			@Override
+			public void run() 
+			{
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				_view.stopTimer();
+				if(_finishedListener != null)
+					_finishedListener.microGameFinishedEvent(true);
+			}
+			
+		});
+		t.start();
+	}
+	
+}
