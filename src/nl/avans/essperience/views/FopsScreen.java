efd3ca1d8da31f83285d240c4c1736ee9754cbe9
Rudy <rudyjason.ep@hotmail.com>
2014-05-27 10:02:19 +0200

    small changes to fops

--- src/nl/avans/essperience/views/FopsScreen.java
+++ src/nl/avans/essperience/views/FopsScreen.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import net.phys2d.raw.Body;
 import nl.avans.essperience.entities.simon.FruitPiece;
+import nl.avans.essperience.main.Main;
 import nl.avans.essperience.models.FopsModel;
 import nl.avans.essperience.utils.AssetManager;
 
@@ -15,10 +16,14 @@ public class FopsScreen extends GameScreen
 	private static final long serialVersionUID = -6529289814578856921L;
 	private Image[] _fruitImages = new Image[4];
 	private Image _crosshair = AssetManager.Instance().getImage("Fops/crosshair.png");
-	public static final int BANANA = 0;
-	public static final int ORANGE = 1;
-	public static final int APPLE = 2;
-	public static final int PEAR = 3;
+	private Image _bullet = AssetManager.Instance().getImage("Fops/bullet.png");
+	private static final int BANANA = 0;
+	private static final int ORANGE = 1;
+	private static final int APPLE = 2;
+	private static final int PEAR = 3;
+	private static int _screenWidth = Main.GAME.getWidth();
+	private static int _screenHeight = Main.GAME.getHeight();
+	private int _amountOfBullets;
 	
 	public FopsScreen(FopsModel model) 
 	{
@@ -28,7 +33,6 @@ public class FopsScreen extends GameScreen
 		_fruitImages[ORANGE] = AssetManager.Instance().getImage("Simon/orange.png");
 		_fruitImages[APPLE] = AssetManager.Instance().getImage("Simon/apple.png");
 		_fruitImages[PEAR] = AssetManager.Instance().getImage("Simon/pear.png");
-		
 		// TODO Auto-generated constructor stub
 	}
 
@@ -36,6 +40,7 @@ public class FopsScreen extends GameScreen
 	public void update() 
 	{
 		_gameModel.update();
+		
 	}
 	
 	public void paintComponent(Graphics g1)
@@ -45,6 +50,7 @@ public class FopsScreen extends GameScreen
 		
 		//System.out.println("drawing!");
 		FopsModel model = (FopsModel)_gameModel;
+		_amountOfBullets = model.getBullets();
 		ArrayList<Body> fruitBodies = model.getBodies();
 		g.drawImage(_crosshair, (int)model.getCursorPosition().x, (int)model.getCursorPosition().y, 20,20, null);
 		for (Body body : fruitBodies)
@@ -63,6 +69,18 @@ public class FopsScreen extends GameScreen
 			// TODO draw fruits
 		}
 		
+		g.drawString("Bullets: ", _screenWidth - 60, _screenHeight-30);
+		if (_amountOfBullets <= 6)
+		{
+			for (int i = _amountOfBullets; i > 0; i--)
+			{
+				g.drawImage(_bullet, _screenWidth-30-(10*i), _screenHeight-30, null);
+			}
+		}
+		else
+		{
+			g.drawString("" + _amountOfBullets, _screenWidth-30, _screenHeight-30);
+		}
 		addTimeBar(g);
 	}
 
