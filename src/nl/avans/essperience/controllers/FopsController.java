471cf0844e9f994e1c40015d35cd94376ca25467
Rudy <rudyjason.ep@hotmail.com>
2014-06-03 11:42:09 +0200

    added red button to all games

--- src/nl/avans/essperience/controllers/FopsController.java
+++ src/nl/avans/essperience/controllers/FopsController.java
@@ -48,6 +48,10 @@ public class FopsController extends GameController
 						}
 					}
 				}
+				if (key == GameKeys.KeySpacebar)
+				{
+					callFinishedListener(false);
+				}
 			}
 			
 			@Override
