ab594f3373697a1fddf8a7aa255a547ef3b1d130
bmsalters <bmsalter@avans.nl>
2014-06-12 15:16:01 +0200

    added dlls

--- src/nl/avans/essperience/main/Main.java
+++ src/nl/avans/essperience/main/Main.java
@@ -4,13 +4,13 @@ import java.awt.Dimension;
 
 import nl.avans.essperience.controllers.GameHandler;
 import nl.avans.essperience.controllers.LightController;
-import nl.avans.essperience.controllers.SerialControllerOLD;
+//import nl.avans.essperience.controllers.SerialControllerOLD;
 
 public class Main 
 {
 	public static GameHandler GAME;
 	public static Dimension DIMENSION = new Dimension(1920, 1080);
-	public static String COM = "ttyACM0";
+	public static String COM = "COM3";
 	public static void main(String[] args)
 	{
 		System.out.println("Started");
